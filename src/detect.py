19e9e71a8c27f9b161ab834302c62095bd70a5b0
jason9075 <jason9075@users.noreply.github.com>
2025-09-28 17:47:30 +0800

    update add default frame thr to 10

--- src/detect.py
+++ src/detect.py
@@ -27,7 +27,7 @@ FACE_CONF_THR = float(os.getenv("FACE_CONF_THR"))
 OFF_HOURS_INTERVAL_SEC = int(
     os.getenv("OFF_HOURS_INTERVAL_SEC", str(INTERVAL_SEC * 3))
 )
-FRAME_DIFF_THR = float(os.getenv("FRAME_DIFF_THR", "5"))
+FRAME_DIFF_THR = float(os.getenv("FRAME_DIFF_THR", "10"))
 
 
 class Person:
